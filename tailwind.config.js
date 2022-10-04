0f995271bc7ed704d898583a33fc6bff53fbe4f9
Duong Phan <duopnh@gmail.com>
2022-10-04 14:23:30 +0700

    Add config color tailwindcss, typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: 'rgb(107 33 168)',
+        secondary: 'rgb(147 51 234)',
+        disable: 'rgb(216 180 254)',
+        hover: 'rgb(167 139 250)'
+      }
     },
   },
   plugins: [],
